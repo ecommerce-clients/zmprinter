5b5c03fcc2ed6ad52a232a52d09a9cfb2b71cca1
Syed Adnan Ahmed <syed.adnanahmed@live.com>
2023-03-07 22:37:33 +0500

    Upload Logo File

--- site/static/javascript/social_links.js
+++ site/static/javascript/social_links.js
@@ -1,20 +1,13 @@
-var fb = "";
-var tw = "";
-var yt = "";
-var insta = "";
-var pt = "";
-
-
-var social_mobile = "03210000000";
-var social_email = "example@gmail.com";
-var social_location = "address";
-var about_info = decodeURIComponent("At brand, we draw our inspirations from the diverse global arena and pack it under one roof, to share the fact that beauty is universal. We constantly strive to challenge the limitations of all the possibilities when it comes to weaving, printing, embroidery & embellishments. The cloth used is not mere fabric for us, we treat it like an artist treats his canvas; with the love, respect and dedication that it deserves. Here at Gul Ahmed, we understand that style and fashion are a vital part of your lives and so we strive to bring it to your doorstep in the easiest possible way. brand motto has always been to provide excellent quality and service.");
-
-
-
-var company_name="Eralive";
-
-var tagmaneger = "GTM-MHJK6NC";
-
-var Receipt_Header = ''; 
-var Receipt_Footer= ';No Refund;No Exchange';
+var fb = ''; 
+ var tw = ''; 
+ var yt = ''; 
+ var insta = ''; 
+ var pt = ''; 
+ var social_mobile = '923094728729' 
+ var social_location = ''; 
+ var social_email = 'zmprinter958@gmail.com'; 
+ var about_info = ''; 
+ var company_name= 'ZM printer '; 
+ var tagmaneger = ''; 
+ var Receipt_Header = ''; 
+ var Receipt_Footer= '';
